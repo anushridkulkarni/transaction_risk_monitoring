7419f839968c10c7c7e0ca0aee31d734f0a357e3
anushridkulkarni <anushridkulkarni@gmail.com>
2026-03-01 17:05:31 +0530

    Changed the ID to UUID

--- surveillance-engine/resources/sql/transaction_schema_01.sql
+++ surveillance-engine/resources/sql/transaction_schema_01.sql
@@ -1,12 +1,13 @@
 CREATE DATABASE `surveillance_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 CREATE TABLE `transactions` (
-                                `id` bigint NOT NULL AUTO_INCREMENT,
-                                `from_account` varchar(255) DEFAULT NULL,
-                                `to_account` varchar(255) DEFAULT NULL,
+                                `id` BINARY(16) NOT NULL,
+                                `from_account` varchar(255) NOT NULL,
+                                `to_account` varchar(255) NOT NULL,
                                 `amount` double NOT NULL,
                                 `transaction_type` varchar(255) DEFAULT NULL,
-                                `risk_score` int DEFAULT '0',
-                                `status` varchar(255) DEFAULT NULL,
-                                `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                                `risk_score` int DEFAULT 0,
+                                `description` varchar(255) DEFAULT NULL,
+                                `status` varchar(255) DEFAULT "INITIATED",
+                                `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                 PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
